a22f11ea3e86161e5213f9eb9218b13d61aa7ad6
tb12show12 <152938700+tb12show12@users.noreply.github.com>
2024-08-31 13:59:17 -0400

    new OG function using firebase

--- netlify/functions/createOGPage.js
+++ netlify/functions/createOGPage.js
@@ -1,7 +1,49 @@
-exports.handler = async (event) => {
-    const { route, data1, data2, title, description, thumbnailUrl } = event.queryStringParameters;
+var admin = require("firebase-admin");
+var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
-    const fullUrl = data2 ? `https://cipher-app.com/${route}/${data1}/${data2}` : `https://cipher-app.com/${route}/${data1}`;
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+const getTripDataFromFirebase = async (tripId) => {
+    try {
+        const tripRef = db.collection('trips').doc(tripId);
+        const tripDoc = await tripRef.get();
+
+        if (!tripDoc.exists) {
+            console.log(`No trip found with ID: ${tripId}`);
+            return null;
+        }
+
+        return tripDoc.data();
+
+    } catch (error) {
+        console.error(`Error fetching trip data: ${error.message}`);
+        return null;
+    }
+};
+
+exports.handler = async (event) => {
+
+    const path = event.path;
+    const pathParts = path.split('/'); // Split the path into parts
+    const route = pathParts[1]; // This should be 'trip-share'
+    const tripId = pathParts[2]; // This should be tripId
+    
+    const fullUrl = `https://cipher-app.com${path}`;
+
+
+    // Function to fetch trip data from Firebase
+    const tripData = await getTripDataFromFirebase(tripId);
+
+    if (!tripData) {
+        return {
+            statusCode: 404,
+            body: 'Trip not found',
+        };
+    }
 
     // Generate the HTML with OG tags
     const metaTags = `
@@ -10,12 +52,11 @@ exports.handler = async (event) => {
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta property="og:title" content="${title}" />
-            <meta property="og:description" content="${description}" />
-            <meta property="og:image" content="${thumbnailUrl}" />
+            <meta property="og:title" content="${tripData.title}" />
+            <meta property="og:description" content="${tripData.shortDescription}" />
+            <meta property="og:image" content="${tripData.thumbnailURL}" />
             <meta property="og:url" content="${fullUrl}" />
             <meta property="og:type" content="website" />
-            <title>${title}</title>
         </head>
         <body>
             <p>This page is meant for social media crawlers to extract meta tags.</p>
