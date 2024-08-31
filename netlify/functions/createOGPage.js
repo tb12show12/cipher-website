23ec85a041c6538569583577002e737dfbe61c01
tb12show12 <152938700+tb12show12@users.noreply.github.com>
2024-08-31 14:30:21 -0400

    Added app store redirect

--- netlify/functions/createOGPage.js
+++ netlify/functions/createOGPage.js
@@ -27,46 +27,77 @@ const getTripDataFromFirebase = async (tripId) => {
 
 exports.handler = async (event) => {
 
-    const path = event.path;
-    const pathParts = path.split('/'); // Split the path into parts
-    const route = pathParts[1]; // This should be 'trip-share'
-    const tripId = pathParts[2]; // This should be tripId
+    try {
+        const APP_STORE_URL = 'https://apps.apple.com/us/app/cipher-travel-memories-shared/id6503364756';
+        const url = `https://cipher-app.com${event.path}`;  // The original Universal Link
+        const userAgent = event.headers['user-agent'];
     
-    const fullUrl = `https://cipher-app.com${path}`;
+        const path = event.path;
+        const pathParts = path.split('/'); // Split the path into parts
+        const route = pathParts[1]; // This should be 'trip-share'
+        
+        let tripId = null;
+        let userId = null;
+    
+        if (pathParts.length === 3){
+            tripId = pathParts[2];
+        } else {
+            userId = pathParts[2];
+            tripId = pathParts[3];
+        }
+        
+        const title = (pathParts[1] === 'trip-share') ? 'Check out this trip on Cipher!' : `You're invited to join this trip on Cipher!`;
+        const description = tripData?.shortDescription || "Join Cipher to capture and share your travel experiences with friends and family";
+        const imageUrl = tripData?.thumbnailURL || "https://cipher-app.com/assets/Butterfly2.png";
+    
+        // Function to fetch trip data from Firebase
+        const tripData = await getTripDataFromFirebase(tripId);
+    
+        if (!tripData) {
+            return {
+                statusCode: 404,
+                body: 'Trip not found',
+            };
+        }
+    
+        // Generate the HTML with OG tags
+        const responseHTML = `
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta property="og:title" content="${title}" />
+                <meta property="og:description" content="${description}" />
+                <meta property="og:image" content="${imageUrl}" />
+                <meta property="og:url" content="${url}" />
+                <meta property="og:type" content="website" />
+            </head>
+            <body>
+                <p>This page is meant for social media crawlers to extract meta tags.</p>
+                <script type="text/javascript">
+                    setTimeout(function() {
+                        console.log("Redirecting now to: '${APP_STORE_URL}'");
+                        window.location = '${APP_STORE_URL}';
+                    }, 150);
+                </script>
+                <noscript><meta http-equiv="refresh" content="0;url=${APP_STORE_URL}"></noscript>
+            </body>
+            </html>
+        `;
 
-
-    // Function to fetch trip data from Firebase
-    const tripData = await getTripDataFromFirebase(tripId);
-
-    if (!tripData) {
         return {
-            statusCode: 404,
-            body: 'Trip not found',
+            statusCode: 200,
+            headers: { 'Content-Type': 'text/html' },
+            body: responseHTML,
+        };
+
+    } catch (error) {
+        console.error("Error occurred in redirect function:", error);
+        return {
+            statusCode: 500,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Internal Server Error',
         };
     }
-
-    // Generate the HTML with OG tags
-    const metaTags = `
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta property="og:title" content="${tripData.title}" />
-            <meta property="og:description" content="${tripData.shortDescription}" />
-            <meta property="og:image" content="${tripData.thumbnailURL}" />
-            <meta property="og:url" content="${fullUrl}" />
-            <meta property="og:type" content="website" />
-        </head>
-        <body>
-            <p>This page is meant for social media crawlers to extract meta tags.</p>
-        </body>
-        </html>
-    `;
-
-    return {
-        statusCode: 200,
-        headers: { 'Content-Type': 'text/html' },
-        body: metaTags,
-    };
 };
